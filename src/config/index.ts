6fca3bb848566f1b84dc3ba61ae02164c82cdfbc
Adrian Catalan <adrian.catalan@mango.com>
2023-10-25 22:11:39 +0200

    feat: refactor fetch html

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,19 +5,8 @@ import { GetSneakers } from "../core/domain/use-cases/getSneakers";
 const cheerioHtmlRetriever = new CheerioHtmlRetriever();
 const nikeEcommerceCrawler = new NikeEcommerceCrawler();
 
-cheerioHtmlRetriever.getHtml("www.nike.com", "/es/w?q=air%20force%201&vst=air%20force")
-    .then(nikeEcommerceCrawler.getSneakersTable)
-    .then((tables) => {
-        let sneakers: Sneaker[] = [];
-        tables.each((_, table) => (sneakers = sneakers.concat(nikeEcommerceCrawler.parseSneakers(table))));
-        return sneakers;
-    })
+const getSneakers = new GetSneakers(cheerioHtmlRetriever, nikeEcommerceCrawler)
+
+getSneakers.execute("https://www.nike.com", "/es/w?q=air%20force%201&vst=air%20force")
     .then((users) => console.log(users))
     .catch((error) => console.log(error));
-
-
-/*const getSneakers = new GetSneakers(cheerioHtmlRetriever, nikeEcommerceCrawler)
-
-getSneakers.execute("www.nike.com", "/es/w?q=air%20force%201&vst=air%20force")
-    .then((users) => console.log(users))
-    .catch((error) => console.log(error));*/
--- src/infrastructure/repositories/CheerioHtmlRetriever.ts
+++ src/infrastructure/repositories/CheerioHtmlRetriever.ts
@@ -1,32 +1,11 @@
 import { HtmlRetriever } from "../../core/repositories/HtmlRetriever";
-import https from "https";
 
 export class CheerioHtmlRetriever implements HtmlRetriever {
 
-    getHtml = async (hostname: string, path: string): Promise<string> =>
-    new Promise((resolve, reject) => {
-        https
-            .get(
-                {
-                    hostname,
-                    path,
-                    method: "GET",
-                },
-                (res) => {
-                    let html = "";
-                    res.on("data", function (chunk) {
-                        html += chunk;
-                    });
-                    res.on("end", function () {
-                        resolve(html);
-                    });
-                }
-            )
-            .on("error", (error) => {
-                console.error(error);
-                reject(error);
-            });
-    });
+    getHtml = async (hostname: string, path: string): Promise<string> => {
+        const response = await fetch(hostname + path, { headers: { 'Content-Type': 'text/html' } })
+        return response.text()
+    }
 }
 
 
